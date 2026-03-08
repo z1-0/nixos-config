fef140664892287aa92c9919b4f292d393e64719
zion <ox7a696f6e@outlook.com>
2026-03-08 14:04:14 +0800

    update nixos common config

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -15,7 +15,6 @@ in
       enable = true;
       type = "fcitx5";
       fcitx5 = {
-        waylandFrontend = true;
         addons = with pkgs; [
           qt6Packages.fcitx5-chinese-addons
           fcitx5-gtk
