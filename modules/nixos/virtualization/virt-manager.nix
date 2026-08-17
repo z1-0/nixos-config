22c23857c4cc2a9a5c240f7c94ae6238988cc8cc
zion <ox7a696f6e@outlook.com>
2026-08-18 00:55:53 +0800

    feat: add osinfo database support to virt-manager

--- modules/nixos/virtualization/virt-manager.nix
+++ modules/nixos/virtualization/virt-manager.nix
@@ -8,12 +8,15 @@ let
 in
 {
   programs.virt-manager.enable = true;
-
   environment = {
     shellAliases.win = "virsh -c qemu:///system start win && virt-viewer -c qemu:///system -f -w win";
 
+    sessionVariables.OSINFO_SYSTEM_DIR = "${pkgs.osinfo-db}/share/osinfo";
+
     systemPackages = [
       pkgs.dnsmasq # exec 'virsh net-autostart default'
+      pkgs.libosinfo
+      pkgs.osinfo-db
       pkgs.virt-viewer
     ];
   };
