072683a267c1f1b0bc899ad74b6fdeff565277db
zion <ox7a696f6e@outlook.com>
2026-08-19 03:23:03 +0800

    feat: add metacubexd webui to mihomo

--- modules/nixos/mihomo.nix
+++ modules/nixos/mihomo.nix
@@ -1,7 +1,9 @@
-{config, ...}: {
+{ config, pkgs, ... }:
+{
   services.mihomo = {
     enable = true;
     tunMode = true;
     configFile = config.age.secrets."mihomo.yaml".path;
+    webui = pkgs.metacubexd;
   };
 }
